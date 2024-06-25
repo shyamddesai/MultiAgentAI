c0cc77c8f067e77c7ebf74c0ec7cf05880e830a7
Kaushik Krishna <88507899+ketaminenymph@users.noreply.github.com>
2024-06-25 12:07:52 +0400

    working json import with news_report.json

--- Frontend/app.py
+++ Frontend/app.py
@@ -1,5 +1,6 @@
-from flask import Flask,render_template, request, url_for, jsonify, make_response
+from flask import Flask,render_template, request, url_for, jsonify, make_response, abort
 import json
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -12,11 +13,26 @@ def data():
       data = json.load(f)
    return render_template('data.html', summary=data['summary'], chart_data=json.dumps(data['data']))
 
+@app.route("/news")
+def news():
+   with open('news.json') as f:
+      data = json.load(f)
+   return render_template('news.html', news_data = data)
+
+@app.route("/news2")
+def news2():
+   with open('reports/news_report.json') as f:
+      data = json.load(f)
+   return render_template('news2.html', news_data = data)
+
+
 @app.errorhandler(404)
 def not_found(error):
     resp = make_response(render_template('error.html'), 404)
     resp.headers['X-Something'] = 'A value'
     return resp
+
+
 
 
 
