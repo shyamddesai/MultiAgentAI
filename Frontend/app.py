2b0ab8c1fad3ad2cbf02aead3e804271b93d08eb
Kaushik Krishna <88507899+ketaminenymph@users.noreply.github.com>
2024-06-30 21:54:05 +0400

    Remove Placeholder elements

--- Frontend/app.py
+++ Frontend/app.py
@@ -14,7 +14,11 @@ def market_prediction():
 @app.route('/news-analysis')
 def news_analysis():
     try:
+<<<<<<< Updated upstream
         with open('news_report.json') as f:
+=======
+        with open('fastAPI/news_report.json') as f:
+>>>>>>> Stashed changes
             news_data = json.load(f)
     except FileNotFoundError:
         news_data = []
@@ -23,6 +27,13 @@ def news_analysis():
 @app.route('/feature-3')
 def feature_3():
     return "Feature 3 Page"
+<<<<<<< Updated upstream
+=======
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("404.html"), 404
+>>>>>>> Stashed changes
 
 if __name__ == '__main__':
     app.run(debug=True)
