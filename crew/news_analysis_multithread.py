baaf31b603b031e47b5287178f0984bb62a6c041
ClarenceZZT <ztzhang0827@gmail.com>
2024-07-17 14:31:34 +0400

    wrap analysis function

--- crew/news_analysis_multithread.py
+++ crew/news_analysis_multithread.py
@@ -5,52 +5,12 @@ from crewai_tools import FileReadTool, BaseTool
 import time
 import glob
 import json
-
-
+os.environ["OPENAI_MODEL_NAME"] = 'gpt-4o'
 file_read_tool = FileReadTool()
-
-directory_path = 'reports/processed_articles/market_trends/'
-all_files = [os.path.join(directory_path, f) for f in os.listdir(directory_path) if os.path.isfile(os.path.join(directory_path, f))]
 
 def chunk_list(lst, n):
     return [lst[i::n] for i in range(n)]
 
-num_chunks = 8
-file_chunks = chunk_list(all_files, num_chunks)
-
-agents = []
-tasks = []
-
-for i, file_chunk in enumerate(file_chunks):
-    agent = Agent(
-        role=f'Economic researcher {i}',
-        goal=f'Read files in chunk {i}, extract insightful and relevant summaries for the oil and gas market, tailored for ADNOC Global Trading.',
-        verbose=True,
-        memory=True,
-        backstory="You work at ADNOC Global Trading as an economic researcher in the oil and gas energy market. "
-        "Your primary objective is to write analytical summaries based on all relevant documents, "
-        "focusing on key insights for ADNOC Global Trading.",
-        tools=[file_read_tool]
-    )
-    task = Task(
-        description=f"Read and give professional summarizations about the following files: {file_chunk}. "
-        "For each file, provide a summary highlighting the key insight relevant to ADNOC global trading or global oil and gas market. ",
-        expected_output='A json file. For each analyzed document, give its title and summary in the following format: '
-        '\{"Title":,"Summary":\}. Use "," to split different summaries and use square bracket to include all summaries. '
-        "Don't give anwser which is not required, for example, filepath of document, conclusion after summaries, word 'json' or character ''' at beginning. ",
-        output_file=directory_path+f'summary/result{i}.json',
-        tools=[file_read_tool],
-        agent=agent
-    )
-    agents.append(agent)
-    tasks.append(task)
-
-crew = Crew(
-    agents=agents,
-    tasks=tasks,
-    process=Process.sequential,
-    memory=True
-)
 def execute_task(task):
     return task.execute()
 
@@ -62,33 +22,80 @@ def kickoff_parallel(crew):
             results.append(future.result())
     return results
 
-parallel_results = kickoff_parallel(crew)
 
+def summerize(category: str, num_chunks: int):
 
+    directory_path = f'reports/processed_articles/{category}'
+    all_files = [os.path.join(directory_path, f) for f in os.listdir(directory_path) if os.path.isfile(os.path.join(directory_path, f))]
+    file_chunks = chunk_list(all_files, num_chunks)
 
-def merge(path):
-    merged_data = []
-    full_path = os.path.join(path, 'summary', 'result*.json')
-    print(full_path)
-    files_to_delete = []
+    agents = []
+    tasks = []
 
-    for filename in glob.glob(full_path):
-        print(filename)
-        with open(filename, 'r') as file:
-            data = json.load(file)
-            merged_data.extend(data)
-        files_to_delete.append(filename)
+    for i, file_chunk in enumerate(file_chunks):
+        agent = Agent(
+            role=f'Economic researcher {i}',
+            goal=f'Read files in chunk {i}, get title and source from the old title, extract insightful and relevant summaries for the oil and gas market, tailored for ADNOC Global Trading.',
+            verbose=True,
+            memory=True,
+            backstory="You work at ADNOC Global Trading as an economic researcher in the oil and gas energy market. "
+            "Your primary objective is to write analytical summaries based on all relevant documents, "
+            "focusing on key insights for ADNOC Global Trading.",
+            tools=[file_read_tool]
+        )
+        task = Task(
+            description=f"Read and give professional summarizations about the following files: {file_chunk}. "
+            "For each file, provide a list of keypoints highlighting the key insight relevant to ADNOC global trading or global oil and gas market. "
+            "Keep the origin title and source",
+            expected_output='A json file. For each analyzed document, give its title and source and keypoints in the following format: '
+            '\{"title":, "source":, "keypoints":[]\}. Use "," to split different summaries and use square bracket to include all summaries. '
+            "Don't give anwser which is not required, for example, filepath of document, conclusion after summaries, word 'json' or character ''' at beginning. ",
+            output_file=directory_path+f'summary/result{i}.json',
+            tools=[file_read_tool],
+            agent=agent
+        )
+        agents.append(agent)
+        tasks.append(task)
 
-    output_path = os.path.join(path, 'summary', 'report.json')
+    crew = Crew(
+        agents=agents,
+        tasks=tasks,
+        process=Process.sequential,
+        memory=True
+    )
+
+    parallel_results = kickoff_parallel(crew)
+    parallel_results = [json.loads(result) for result in parallel_results]
+    parallel_results = [entry for results in parallel_results for entry in results]
+
+    output_path = os.path.join(directory_path, 'summary', 'report.json')
     with open(output_path, 'w') as file:
-        json.dump(merged_data, file, indent=4)
+        json.dump(parallel_results, file, indent=4)
 
-    # for file in files_to_delete:
-    #     os.remove(file)
 
-    return output_path
+# def merge(path):
+#     merged_data = []
+#     full_path = os.path.join(path, 'summary', '*.json')
+#     print(full_path)
+#     files_to_delete = []
 
-merge(directory_path)
+#     for filename in glob.glob(full_path):
+#         print(filename)
+#         with open(filename, 'r') as file:
+#             data = json.load(file)
+#             merged_data.extend(data)
+#         files_to_delete.append(filename)
+
+#     output_path = os.path.join(path, 'report.json')
+#     with open(output_path, 'w') as file:
+#         json.dump(merged_data, file, indent=4)
+
+#     # for file in files_to_delete:
+#     #     os.remove(file)
+
+#     return output_path
+
+# merge(directory_path)
 # parallel_results = [sum(parallel_results[i:i+4], []) for i in range(0, len(parallel_results), 4)]
 # result_chunks = chunk_list(parallel_results)
 
